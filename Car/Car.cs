b4d17959a91a1f2a43c637134f5802fb7ef08cc8
test <test@KAB42-3>
2022-09-30 11:46:05 +0300

    Добавьте файлы проекта.

---
+++ Car/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    internal class Car
+    {
+        private string _model;//модель
+        private string _stamp;//марка
+        private string _price;//цена
+        private int _enginepower; //мощность двигателя
+        private int _fuelconsumption;//расход топлива в час
+        private int _maximumspeed;//максимальная скорость
+        private int _gasolinelevel;//уровень бензина
+        public void ToStart()
+        {
+
+        }
+        public void GetInfo()
+        {
+
+        }
+        public void Upgrade()
+        {
+
+        }
+    }
+}
